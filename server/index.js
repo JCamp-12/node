1f6871b78d834522a3b089e275fabcec33fe701c
JCamp-12 <JChandlerCampbell@gmail.com>
2019-11-19 00:19:57 -0500

    server, heroku, and some preFirstDeploy

--- server/index.js
+++ server/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express()
 
 app.get('/', (req, res) => {
+  console.log('its working!')
   res.send({ hi: 'there' })
 })
+
+const PORT = process.env.PORT
 
 app.listen(5000)
